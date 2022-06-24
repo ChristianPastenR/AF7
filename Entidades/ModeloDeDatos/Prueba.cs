267b54eb703666911fb64dfca4a03f5c260f3874
ChristianPastenR <93354953+ChristianPastenR@users.noreply.github.com>
2022-06-23 22:46:07 -0400

    Add files via upload

---
+++ Entidades/ModeloDeDatos/Prueba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.ModeloDeDatos
+{
+    class Prueba
+    {
+        Cargo entidades = null;
+
+        public Cargo Entidades
+        {
+            get
+            {
+                if(entidades == null)
+                    entidades = new Cargo();
+                return entidades;
+
+            }
+
+        }
+    }
+}
